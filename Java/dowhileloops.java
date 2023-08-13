b29970c3243cd2f074faa1d36a9a08d7cc637554
Equarius <72136153+rakibhassan66@users.noreply.github.com>
2023-08-13 18:10:35 +0600

    Add files via upload

---
+++ Java/dowhileloops.java
@@ -0,0 +1,9 @@
+public class dowhileloops {
+    public static void main(String[] args) {
+        int foga=1;
+        do{
+            System.out.println(foga+" Kakar bangladesh");
+            foga++;
+        }while(foga<=10);
+    }
+}
