6e6a61984d26220543bce0823bfa9015ab71c638
Equarius <72136153+rakibhassan66@users.noreply.github.com>
2023-08-19 01:47:13 +0600

    Add files via upload

---
+++ C#/Hello/Variable/Variable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tr = 12;
+            Console.WriteLine(tr);
+            Console.ReadLine();
+        }
+    }
+}
