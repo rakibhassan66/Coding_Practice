6138385ca0175c227d3d1da37fb208ae4c149e9a
Equarius <72136153+rakibhassan66@users.noreply.github.com>
2023-08-19 01:46:09 +0600

    Add files via upload

---
+++ C#/Hello/Typecasting/Typecasting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typecasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert your number ");
+            int a = 1;
+            float b = 34.2F;
+            double c = 198484.5988744984D;
+            bool tr = true;
+            bool rd = false;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(tr);
+            Console.WriteLine(tr);
+            Console.ReadLine();
+        }
+    }
+}
