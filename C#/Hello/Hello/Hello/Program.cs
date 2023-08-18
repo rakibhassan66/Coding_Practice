39e977d6fcfbe7e0aa5f01b2236b2f20cd739a2f
Equarius <72136153+rakibhassan66@users.noreply.github.com>
2023-08-19 01:45:28 +0600

    Add files via upload

---
+++ C#/Hello/Hello/Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C#/Hello/input/input/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert your text here ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Welcome " +name);
+            String ty = Console.ReadLine();
+            Console.WriteLine(ty);
+            Console.ReadLine();
+        }
+    }
+}
