35d34dbb5727d550349f0008b3a6963f7b5dcbc2
Equarius <72136153+rakibhassan66@users.noreply.github.com>
2023-08-19 01:44:48 +0600

    Add files via upload

---
+++ C#/Hello/datatypes/datatypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String rd = Console.ReadLine();
+
+            Console.WriteLine("The number is " + rd);
+            Console.ReadLine();
+        }
+    }
+}
